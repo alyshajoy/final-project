1eef35ed4a0c96c465048dd38186fcd19fffe6f3
Alysha Huot <alyshajhuot@gmail.com>
2024-04-29 23:38:12 -0600

    set up express and database connection

---
+++ db/connection.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+// Database connection parameters
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
+
+const db = new Pool(dbParams);
+
+db.connect(err => {
+  if (err) {
+    console.error('Connection error', err.stack);
+  } else {
+    console.log('Connected to database');
+  }
+});
+
+module.exports = db;
