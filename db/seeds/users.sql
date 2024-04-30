06a70f005f8a8de5e384545a77c122a5a5a3aba3
Alysha Huot <alyshajhuot@gmail.com>
2024-04-30 00:08:48 -0600

    create seed data for all tables

---
+++ db/seeds/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password_hash, created_at, last_login, username)
+VALUES
+('John', 'Doe', 'john.doe@example.com', 'hashedpassword1', NOW(), NOW(), 'johndoe'),
+('Jane', 'Smith', 'jane.smith@example.com', 'hashedpassword2', NOW(), NOW(), 'janesmith'),
+('Alice', 'Johnson', 'alice.johnson@example.com', 'hashedpassword3', NOW(), NOW(), 'alicejohnson');
