d2422b8b35e4f29e49dd0f26928a3405ac58783e
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-02 16:05:22 -0700

    Use the router to import all the routes from the tasks file into the main server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ require('dotenv').config({ path: '../.env'});
 // Modules
 const express = require('express');
 const db = require('./db/connection'); // Assuming you have a db module for database connection
+const tasksRoutes = require('./routes/tasks');
 
 
 // Initialize Express
@@ -14,18 +15,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// All routes from the Tasks feature
+app.use('/api/tasks', tasksRoutes)
+
 // Index Route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Tasks Route
-app.get('/tasks', (req, res) => {
-  db.query(`SELECT * FROM tasks`)
-  .then(({rows}) => {
-      res.json(rows);
-    });
-});
+
 
 // Start the server
 const port = process.env.PORT || 3001;
