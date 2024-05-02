0823df4c5b42a27ad614a88aa4d102d3d5cab57f
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-02 14:03:04 -0700

    Move individual feature components to their own pages

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Main from "../components/Main";
 
 const Tasks = () => {
   return (
     <div>
       <h1>I am the Tasks page</h1>
+      <div>
+        <Main></Main>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import FocusTimer from '../components/FocusTimer'
 
 const Timer = () => {
   return (
     <div>
       <h1>I am the Timer page</h1>
+      <main>Hello World!
+        <FocusTimer />
+      </main>
     </div>
   );
 };
