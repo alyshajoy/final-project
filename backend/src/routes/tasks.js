cf7f6bebe3477cdec30b26f80c52a61968da46d0
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-02 18:23:55 -0700

    Add a temp post route

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -6,11 +6,19 @@ const router = express.Router();
 //Prefix '/api/tasks' for endpoint
 
 // Tasks Routes
+
+//Get 
 router.get('/', (req, res) => {
   db.query(`SELECT * FROM tasks`)
   .then(({rows}) => {
       res.json(rows);
     });
 });
+
+//Post
+router.post('/', (req,res) => {
+  db.query(`INSERT INTO tasks(id, user_id, title, description, priority, due_date) VALUES($1, $2, $3, $4, $5, $6)`, 
+  [id, user_id, title, description, priority, due_date])
+})
 
 module.exports = router;
