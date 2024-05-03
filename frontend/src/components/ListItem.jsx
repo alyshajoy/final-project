ff45615a63ce21533811e594186c7dceeb9a95d9
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-03 15:27:56 -0700

    Add List components

---
+++ frontend/src/components/ListItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListItem = () => {
+  return (
+    <div>
+      <h1>I am the ListItem</h1>
+    </div>
+  );
+};
+
+export default ListItem;
---
+++ frontend/src/components/ListItems.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListItems = () => {
+  return (
+    <div>
+      <h1>I am the ListItems</h1>
+    </div>
+  );
+};
+
+export default ListItems;
