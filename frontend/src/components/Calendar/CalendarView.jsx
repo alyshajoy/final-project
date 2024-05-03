f2935e7a11ea5cbd39a98e3461a9ab7215c0faa4
Alysha Huot <alyshajhuot@gmail.com>
2024-05-03 10:06:06 -0600

    add functionality to change views between month, week and day

--- frontend/src/components/Calendar/CalendarView.jsx
+++ frontend/src/components/Calendar/CalendarView.jsx
@@ -3,31 +3,32 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import ViewSelector from './ViewSelector';
 
 function CalendarView() {
+    const [currentView, setCurrentView] = useState('dayGridMonth');
 
-  const [currentView, setCurrentView] = useState('dayGridMonth');
-    
-  function handleDateClick(arg) {
-       alert('Date clicked: ' + arg.dateStr);
-  }
+    function handleDateClick(arg) {
+        alert('Date clicked: ' + arg.dateStr);
+    }
 
-  console.log("Current View:", currentView);
-
-
-  return (
-      <div>
-        <ViewSelector currentView={currentView} setCurrentView={setCurrentView} />
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
-          weekends={true}
-          events={[{ title: 'Sample Event', start: new Date() }]}
-          dateClick={handleDateClick}
-        />
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <button onClick={() => setCurrentView('dayGridMonth')}>Month</button>
+                <button onClick={() => setCurrentView('timeGridWeek')}>Week</button>
+                <button onClick={() => setCurrentView('timeGridDay')}>Day</button>
+            </div>
+            
+            <FullCalendar
+                key={currentView}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView={currentView}
+                weekends={true}
+                events={[{ title: 'Sample Event', start: new Date() }]}
+                dateClick={handleDateClick}
+            />
+        </div>
+    );
 }
 
 export default CalendarView;
