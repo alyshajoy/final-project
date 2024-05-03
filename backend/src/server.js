c66f0477e928967be8916448aeb3c68528ae9edc
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-02 17:19:31 -0700

    Import all the routes from their respective files and use them in the server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,24 +5,30 @@ require('dotenv').config({ path: '../.env'});
 // Modules
 const express = require('express');
 const db = require('./db/connection'); // Assuming you have a db module for database connection
-const tasksRoutes = require('./routes/tasks');
 
+// Require/import Feature Routes
+const tasksRoutes = require('./routes/tasks');
+const timerRoutes = require('./routes/timer');
+const calendarRoutes = require('./routes/calendar');
+const badgesRoutes = require('./routes/badges');
 
 // Initialize Express
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// All routes from the Tasks feature
+// Define endpoints
 app.use('/api/tasks', tasksRoutes)
+app.use('/api/timer', timerRoutes)
+app.use('/api/calendar', calendarRoutes)
+app.use('/api/badges', badgesRoutes)
 
 // Index Route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
 
 
 // Start the server
