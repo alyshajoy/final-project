1f058f807da4dcd83e9daf53d31639b93cfa9cca
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-05 15:33:56 -0700

    Create remove task component

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Checkbox from "../pages/tasks/Checkbox";
+import Checkbox from "./buttons/Checkbox";
 import '../styles/ListItems.scss';
+import RemoveTask from "./buttons/RemoveTask";
 
 const ListItem = (props) => {
   return (
     <div className="list-item-container">
       <span><Checkbox/></span>
       <h3 className="list-item-title"> {props.item}</h3>
-      
+      <span><RemoveTask/></span>
     </div>
   );
 };
---
+++ frontend/src/components/buttons/RemoveTask.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const RemoveTask = () => {
+  return (
+    <>
+      <i className="fa-solid fa-circle-minus"></i>
+    </>
+  )
+};
+
+export default RemoveTask;
