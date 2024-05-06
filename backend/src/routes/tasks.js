3c67fe666d1d4f7fb89ec7b6d3d03fdaf5b34bef
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-06 15:36:12 -0700

    Update insert query to log the added task to the console

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -17,8 +17,14 @@ router.get('/', (req, res) => {
 
 //Post
 router.post('/', (req,res) => {
-  db.query(`INSERT INTO tasks(id, user_id, title, description, priority, due_date) VALUES($1, $2, $3, $4, $5, $6)`, 
-  [id, user_id, title, description, priority, due_date])
+  db.query(`
+  INSERT INTO tasks(task_id, user_id, title, description, priority, due_date, status) 
+  VALUES($1, $2, $3, $4, $5, $6, $7)
+  RETURNING *;
+  `,[id, user_id, title, description, priority, due_date, status])
+  .then((taskEntry) => {
+    console.log(taskEntry);
+  })
 })
 
 module.exports = router;
