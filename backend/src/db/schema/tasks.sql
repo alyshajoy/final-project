ed3a2c2a054a4d8ed793178d8818d89888cd748d
Pihu Gupta <pihugupta0501@gmail.com>
2024-05-07 13:18:34 -0700

    Rename all id fields and edit a few fields in tasks table

--- backend/src/db/schema/tasks.sql
+++ backend/src/db/schema/tasks.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS tasks CASCADE;
 
 CREATE TABLE tasks (
-    task_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     priority INTEGER,
     due_date DATE,
-    status BOOLEAN,
+    completed BOOLEAN,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
