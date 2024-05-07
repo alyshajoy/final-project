ba416200798be347073b0c1098de166e478645d6
Faelixx <sasha.taylor7@gmail.com>
2024-05-07 10:17:58 -0600

    Feat: Add timer state manipulation via useState

--- frontend/src/components/timer/SetPomodoro.jsx
+++ frontend/src/components/timer/SetPomodoro.jsx
@@ -1,8 +1,14 @@
-
+import { useState } from "react"
 import TimerButton from "./TimerButton"
 
 const SetPomodoro = () => {
-
+  // Default vaulues for pomodoro timer, active key determines which time will run
+  const [newTimer, setNewTimer] = useState({
+    work: 0.3,
+    shortbreak: 0.2,
+    longbreak: 1,
+    active: 'work'
+  });
   return (
     <div className="pomodoro-form-container">
 
