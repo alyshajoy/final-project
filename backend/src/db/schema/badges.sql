b01b6f993b57d9e2aeebbef0ae76a07bfdeb98d1
Fatbobot <Fatbobot@gmail.com>
2024-05-09 22:17:00 -0300

    add status row to badges table

--- backend/src/db/schema/badges.sql
+++ backend/src/db/schema/badges.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS badges CASCADE;
 
 CREATE TABLE badges (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     threshold INTEGER,
-    image_url TEXT
+    image_url TEXT NOT NULL,
+    status BOOLEAN DEFAULT TRUE NOT NULL
 );
--- backend/src/db/websockets/websockets.js
+++ backend/src/db/websockets/websockets.js
@@ -1,6 +1,5 @@
 const WebSocket = require('ws');
 const db = require('../connection'); // Assuming this is your PostgreSQL database connection module
-
 // Create a WebSocket server
 module.exports = (server) => {
   const wss = new WebSocket.Server({ server});
@@ -21,12 +20,31 @@ module.exports = (server) => {
 
     // When a database change event occurs, broadcast the change to WebSocket clients
     db.on('notification', (msg) => {
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(msg));
-        }
-      });
-    });
+      if (msg.channel === 'badge_count_exceeded') {
+          try {
+              (async () => {
+                const result = await db.query('SELECT * FROM badges WHERE id = $1', [msg.payload]);
+              })();
+              const badge = result.rows[0];
+              const payload = {
+                  type: 'badge-earned',
+                  badge: {
+                      imageUrl: badge.image_url,
+                      title: badge.name,
+                      description: badge.description
+                  }
+              };
+              // Send badge data to frontend
+              wss.clients.forEach(client => {
+                  if (client.readyState === WebSocket.OPEN) {
+                      client.send(JSON.stringify(payload));
+                  }
+              });
+          } catch (error) {
+              console.error('Error fetching badge:', error);
+          }
+      }
+   });
   };
 
   // Start listening for database changes
