194cf5d2283014df746f9605146b545dfda2f436
Faelixx <sasha.taylor7@gmail.com>
2024-05-09 22:26:17 -0600

    add home button to timer page

--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FocusTimer from '../components/timer/FocusTimer'
 import TimerSettingsContextProvider from "../contexts/TimerSettingsContext";
+import HomeButton from "../components/buttons/HomeButton";
 
 const Timer = () => {
   return (
     <div>
       <main>
         <TimerSettingsContextProvider>
+          <HomeButton />
           <FocusTimer />
         </TimerSettingsContextProvider>
       </main>
