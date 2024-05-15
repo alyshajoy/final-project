0e00b369806e6f918fa9bcc1f5dd7a576f649896
Alysha Huot <alyshajhuot@gmail.com>
2024-05-15 07:45:20 -0600

    setup component and SCSS files for Home Page

---
+++ frontend/src/components/home/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>ADHD Toolkit</h1>
+      <p>Enhancing focus and organization for ADHD minds</p>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/CSS/Home.css';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>ADHD Toolkit</h1>
       <ul>
         <li><Link to="/badges">Badges</Link></li>
         <li><Link to="/timer">Timer</Link></li>
         <li><Link to="/calendar">Calendar</Link></li>
         <li><Link to="/tasks">Tasks</Link></li>
-        <li><Link to="/register">Register</Link></li>
       </ul>
     </div>
   );
