662ad1b0cafd83b8e7efb56891542ce7fcf4cf76
Fatbobot <Fatbobot@gmail.com>
2024-05-15 16:44:10 -0300

    install react conffeti add triggers for timer (commented out), calendar, tasks fix image rendering bug in badge collection and badge notification adjust styling for notifications

--- backend/src/db/seeds/user_badges.sql
+++ backend/src/db/seeds/user_badges.sql
@@ -1,6 +1,6 @@
 INSERT INTO user_badges (user_id, badge_id, status, earned_date)
 VALUES
-(1, 1, false,NOW()),
+(1, 1, true,NOW()),
 (1, 2, false ,NOW()),
-(1, 3, true ,NOW()),
+(1, 3, false ,NOW()),
 (1, 4, true ,NOW());
