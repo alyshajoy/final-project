a46b8260105dfe8d161063c588b761de867ae417
Fatbobot <Fatbobot@gmail.com>
2024-05-14 21:23:37 -0300

    add backend routes for badge collection add db reset function to activate trigger functions populate user_badges as join table between user and badges

--- frontend/src/pages/Badges.jsx
+++ frontend/src/pages/Badges.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BadgeCollection from "../components/BadgeCollection";
 
 const Badges = () => {
+  const [badgesData, setBadgesData] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/api/badges/')
+      .then(response => response.json())
+      .then(data => setBadgesData(data))
+      .then(console.log('Success:', badgesData))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div>
-      <h1>I am the Badges page</h1>
+      <h1>Badges</h1>
       <header className="App-header">
-      <h1>Achievement Notifications Demo</h1>
-      <BadgeCollection />
+      <BadgeCollection badgesData={badgesData}/>
     </header>
     </div>
     
