2a67d6307c4e262a1482b12d9317c245036b4d88
Faelixx <sasha.taylor7@gmail.com>
2024-05-15 23:27:14 -0600

    Fix: users table default both timer values

--- backend/src/db/seeds/users.sql
+++ backend/src/db/seeds/users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (first_name, last_name, email, password_hash, created_at, last_login, username, timer_active)
+INSERT INTO users (first_name, last_name, email, password_hash, created_at, last_login, username)
 VALUES
-('Jane', 'Smith', 'jane.smith@example.com', 'hashedpassword2', NOW(), NOW(), 'janesmith', false),
-('John', 'Doe', 'john.doe@example.com', 'hashedpassword1', NOW(), NOW(), 'johndoe', false),
-('Alice', 'Johnson', 'alice.johnson@example.com', 'hashedpassword3', NOW(), NOW(), 'alicejohnson', false),
-('Trish', 'Ramberan', 'trish.ramberan@example.com', 'hashedpassword3', NOW(), NOW(), 'trishramberan', false);
+('Jane', 'Smith', 'jane.smith@example.com', 'hashedpassword2', NOW(), NOW(), 'janesmith'),
+('John', 'Doe', 'john.doe@example.com', 'hashedpassword1', NOW(), NOW(), 'johndoe'),
+('Alice', 'Johnson', 'alice.johnson@example.com', 'hashedpassword3', NOW(), NOW(), 'alicejohnson'),
+('Trish', 'Ramberan', 'trish.ramberan@example.com', 'hashedpassword3', NOW(), NOW(), 'trishramberan');
