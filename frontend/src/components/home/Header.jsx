db050c15d3322057e1c2a27edf621e07a368cd25
Faelixx <sasha.taylor7@gmail.com>
2024-05-22 20:28:59 -0600

    Feat: change main header font

--- frontend/src/components/home/Header.jsx
+++ frontend/src/components/home/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="home-header">
       <img src={require('../../assets/homepage_icons/app-icon.png')} alt={"Logo of two hands held up as though focusing on something in front of them."} />
-      <h1>ADHD Toolkit</h1>
+      <h1>Ease</h1>
       <p>Enhancing focus and organization for ADHD minds</p>
     </header>
   );
