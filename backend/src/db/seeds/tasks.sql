adee8e9e89a8a2b5be1b64f9f92603de61eb8bf4
Alysha Huot <alyshajhuot@gmail.com>
2024-05-22 21:40:33 -0600

    change Ease font

--- backend/src/db/seeds/tasks.sql
+++ backend/src/db/seeds/tasks.sql
@@ -5,5 +5,5 @@ VALUES
 (1, 'Update Resume', 'Update the resume with the latest job experiences.', 3, '2024-05-20', 'true', NOW(), NOW()),
 (1, 'Review Code', 'Conduct a thorough review of the new feature code.', 1, '2024-05-05', 'false', NOW(), NOW()),
 (1, 'Respond to Emails', 'Discuss project progress and allocate resources for the next phase.', 2, '2024-05-08', 'true', NOW(), NOW()),
-(1, 'Finsh Reading Chapter', 'Address the top priority bugs reported by the QA team.', 1, '2024-05-12', 'false', NOW(), NOW());
+(1, 'Finish Reading Chapter', 'Address the top priority bugs reported by the QA team.', 1, '2024-05-12', 'false', NOW(), NOW());
 
